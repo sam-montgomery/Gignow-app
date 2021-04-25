bb3da57a0cde29ed0a2037d0ce902e67667d38d4
“40228587” <40228587@qub.ac.uk>
2021-04-25 13:33:18 +0100

    Removed Buttons from card page

--- gignow/lib/artist_cards/swipe_feed_page.dart
+++ gignow/lib/artist_cards/swipe_feed_page.dart
@@ -27,45 +27,15 @@ class _SwipeFeedPageState extends State<SwipeFeedPage> {
                 leading: IconButton(
                     onPressed: () {},
                     icon: Icon(Icons.settings, color: Colors.grey)),
-                actions: <Widget>[
-                  IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.question_answer, color: Colors.grey)),
-                ],
               ),
               backgroundColor: Colors.white,
               body: Column(children: <Widget>[
                 CardsSectionAlignment(artists, context),
-                buttonsRow(),
               ]),
             );
           } else {
             return Loading();
           }
         });
-  }
-
-  Widget buttonsRow() {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 30.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(padding: EdgeInsets.only(right: 8.0)),
-          FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: Colors.white,
-            child: Icon(Icons.close, color: Colors.red),
-          ),
-          Padding(padding: EdgeInsets.only(right: 8.0)),
-          FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: Colors.white,
-            child: Icon(Icons.star, color: Colors.blue),
-          ),
-        ],
-      ),
-    );
   }
 }
