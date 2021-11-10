f6b23e90e2a684d6575d5df5aa48a6e6ac56e538
40229998 <smontgomery27@qub.ac.uk>
2021-11-10 17:37:39 +0000

    Events changes

--- gignow/lib/ui/events/new_events_screen.dart
+++ gignow/lib/ui/events/new_events_screen.dart
@@ -16,6 +16,8 @@ import 'package:gignow/ui/loading.dart';
 import 'package:gignow/widgets/event_list.dart';
 import 'package:numberpicker/numberpicker.dart';
 import 'package:contained_tab_bar_view/contained_tab_bar_view.dart';
+import 'package:duration_picker/duration_picker.dart';
+import 'package:multi_select_flutter/multi_select_flutter.dart';
 
 class NewEventScreen extends StatefulWidget {
   UserModel profile;
@@ -35,43 +37,90 @@ class NewEventScreenState extends State<NewEventScreen> {
   @override
   Widget build(BuildContext context) {
     DateTime eventStart;
-    int duration = 15;
+    Duration _duration = Duration(hours: 0, minutes: 0);
 
     Container detailsCont = new Container(
       child: Column(
         children: [
-          TextButton(
-              onPressed: () {
-                DatePicker.showDateTimePicker(context,
-                    showTitleActions: true,
-                    minTime: DateTime.now(),
-                    maxTime: DateTime.now().add(Duration(days: 7)),
-                    onChanged: (date) {}, onConfirm: (date) {
-                  print('confirm $date');
-                  eventStart = date;
-                }, currentTime: DateTime.now(), locale: LocaleType.en);
-              },
-              child: Text('Event Start Time')),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: Icon(Icons.remove),
-                onPressed: () => setState(() {
-                  final newValue = duration - 15;
-                  duration = newValue.clamp(0, 100);
-                }),
-              ),
-              Text('Current int value: $duration'),
-              IconButton(
-                icon: Icon(Icons.add),
-                onPressed: () => setState(() {
-                  duration = (duration + 15).clamp(0, 100);
-                  print(duration);
-                }),
-              ),
-            ],
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextButton(
+                onPressed: () {
+                  DatePicker.showDateTimePicker(context,
+                      showTitleActions: true,
+                      minTime: DateTime.now(),
+                      maxTime: DateTime.now().add(Duration(days: 7)),
+                      onChanged: (eventStart) {}, onConfirm: (eventStart) {
+                    print('confirm $eventStart');
+                  }, currentTime: DateTime.now(), locale: LocaleType.en);
+                },
+                child: Text('Event Start Time')),
           ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextButton(
+                onPressed: () async {
+                  _duration = await showDurationPicker(
+                    context: context,
+                    initialTime: Duration(hours: 1),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Duration: $_duration')));
+                },
+                child: Text('Event Duration')),
+          ),
+        ],
+      ),
+    );
+
+    List<String> _genres = ["Pop", "Rock", "Metal", "Accoustic", "Folk"];
+
+    List<String> selectedGenres;
+
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.white;
+    }
+
+    final genreSelector = MultiSelectChipField(
+      items: _genres
+          .map((genre) => MultiSelectItem<String>(genre, genre))
+          .toList(),
+      title: Text("Genres:"),
+      headerColor: Colors.blue.withOpacity(0.5),
+      decoration: BoxDecoration(
+        border: Border.all(color: kButtonBackgroundColour, width: 1.8),
+      ),
+      selectedChipColor: Colors.blue.withOpacity(0.5),
+      selectedTextStyle: TextStyle(color: Colors.blue[800]),
+      onTap: (values) {
+        selectedGenres = values;
+      },
+    );
+    bool genreOffer = false;
+
+    Container offersCont = new Container(
+      child: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Checkbox(
+                  value: genreOffer,
+                  checkColor: Colors.white,
+                  fillColor: MaterialStateProperty.resolveWith(getColor),
+                  onChanged: (bool newValue) {
+                    setState(() {
+                      genreOffer = newValue;
+                      print(genreOffer);
+                    });
+                  })),
         ],
       ),
     );
@@ -118,7 +167,7 @@ class NewEventScreenState extends State<NewEventScreen> {
                         color: Colors.black,
                       )
                     ],
-                    views: [detailsCont, Container(color: Colors.green)],
+                    views: [detailsCont, offersCont],
                     onChange: (index) => print(index),
                   ),
                 )),
