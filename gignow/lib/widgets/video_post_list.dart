3399c866a0dac8802b80477c06a0c7b8f3b16d23
40231256 <rmcsorley06@qub.ac.uk>
2021-12-10 15:51:39 +0000

    Scrollable Home feed

--- gignow/lib/widgets/video_post_list.dart
+++ gignow/lib/widgets/video_post_list.dart
@@ -20,6 +20,113 @@ class _VideoPostListState extends State<VideoPostList> {
   List<Future> futures = [];
   int futureIndex = 1;
   Global global = Global();
+  int curVid = Global().vidIdx;
+  final _controller = PageController(initialPage: Global().vidIdx);
+  // @override
+  // Widget build2(BuildContext context) {
+  //   futures.add(firebaseService.getFollowingVideoPosts());
+  //   futures.add(firebaseService.getVideoPosts());
+  //   return FutureBuilder(
+  //     future: showFollowing
+  //         ? (firebaseService.getFollowingVideoPosts())
+  //         : VideoRankingService().getRankedVideos(auth.currentUser.uid),
+  //     // : firebaseService.getVideoPosts(),
+  //     builder: (context, snapshot) {
+  //       if (snapshot.hasData &&
+  //           snapshot.connectionState == ConnectionState.done) {
+  //         List<VideoPost> posts = snapshot.data;
+  //         if (posts.length > 0) {
+  //           VideoPostWidget curVid = VideoPostWidget(
+  //               posts[showFollowing ? global.followingVidIdx : global.vidIdx]);
+  //           //previous vid, next vid
+  //           return Stack(
+  //             children: [
+  //               curVid,
+  //               //VideoPostWidget(posts[vidIndex]),
+  //               Column(children: [
+  //                 SizedBox(
+  //                   height: 30,
+  //                 ),
+  //                 Row(
+  //                   mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //                   children: [
+  //                     MaterialButton(
+  //                       color: Colors.white,
+  //                       child: Text("Previous"),
+  //                       onPressed: () {
+  //                         if (showFollowing) {
+  //                           if (global.followingVidIdx > 0) {
+  //                             setState(() {
+  //                               global.followingVidIdx--;
+  //                             });
+  //                           }
+  //                         } else {
+  //                           if (global.vidIdx > 0) {
+  //                             setState(() {
+  //                               global.vidIdx--;
+  //                             });
+  //                           }
+  //                         }
+  //                         //widget.createState();
+  //                       },
+  //                     ),
+  //                     MaterialButton(
+  //                       color: Colors.white,
+  //                       child:
+  //                           Text(showFollowing ? "Show All" : "Show Following"),
+  //                       onPressed: () {
+  //                         setState(() {
+  //                           showFollowing = !showFollowing;
+  //                           // global.vidIdx = 0;
+  //                           if (futureIndex == 0) {
+  //                             futureIndex = 1;
+  //                           } else {
+  //                             futureIndex = 0;
+  //                           }
+  //                         });
+  //                       },
+  //                     ),
+  //                     MaterialButton(
+  //                       color: Colors.white,
+  //                       child: Text("Next"),
+  //                       onPressed: () {
+  //                         if (showFollowing) {
+  //                           if (global.followingVidIdx < posts.length - 1) {
+  //                             setState(() {
+  //                               global.followingVidIdx++;
+  //                             });
+  //                           }
+  //                         } else {
+  //                           if (global.vidIdx < posts.length - 1) {
+  //                             setState(() {
+  //                               global.vidIdx++;
+  //                             });
+  //                           }
+  //                         }
+  //                         //widget.createState();
+  //                       },
+  //                     ),
+  //                   ],
+  //                 ),
+  //               ])
+  //             ],
+  //           );
+  //           // return ListView.builder(
+  //           //     shrinkWrap: true,
+  //           //     itemCount: posts.length,
+  //           //     itemBuilder: (context, index) {
+  //           //       VideoPost post = posts[index];
+  //           //       return VideoPostWidget(post);
+  //           //     });
+  //         } else {
+  //           return Container();
+  //         }
+  //       } else {
+  //         return Loading();
+  //       }
+  //     },
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -38,85 +145,49 @@ class _VideoPostListState extends State<VideoPostList> {
             VideoPostWidget curVid = VideoPostWidget(
                 posts[showFollowing ? global.followingVidIdx : global.vidIdx]);
             //previous vid, next vid
-            return Stack(
-              children: [
-                curVid,
-                //VideoPostWidget(posts[vidIndex]),
-                Column(children: [
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      MaterialButton(
-                        color: Colors.white,
-                        child: Text("Previous"),
-                        onPressed: () {
-                          if (showFollowing) {
-                            if (global.followingVidIdx > 0) {
-                              setState(() {
-                                global.followingVidIdx--;
-                              });
-                            }
-                          } else {
-                            if (global.vidIdx > 0) {
-                              setState(() {
-                                global.vidIdx--;
-                              });
-                            }
-                          }
-                          //widget.createState();
-                        },
+
+            return PageView.builder(
+                scrollDirection: Axis.vertical,
+                controller: _controller,
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  VideoPost post = posts[index];
+                  return Stack(children: [
+                    VideoPostWidget(post),
+                    Column(children: [
+                      SizedBox(
+                        height: 30,
                       ),
-                      MaterialButton(
-                        color: Colors.white,
-                        child:
-                            Text(showFollowing ? "Show All" : "Show Following"),
-                        onPressed: () {
-                          setState(() {
-                            showFollowing = !showFollowing;
-                            // global.vidIdx = 0;
-                            if (futureIndex == 0) {
-                              futureIndex = 1;
-                            } else {
-                              futureIndex = 0;
-                            }
-                          });
-                        },
-                      ),
-                      MaterialButton(
-                        color: Colors.white,
-                        child: Text("Next"),
-                        onPressed: () {
-                          if (showFollowing) {
-                            if (global.followingVidIdx < posts.length - 1) {
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          FlatButton(
+                            color: Colors.transparent,
+                            child: Text(
+                              showFollowing ? "Show All" : "Show Following",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            onPressed: () {
                               setState(() {
-                                global.followingVidIdx++;
+                                showFollowing = !showFollowing;
+                                // global.vidIdx = 0;
+                                if (futureIndex == 0) {
+                                  futureIndex = 1;
+                                } else {
+                                  futureIndex = 0;
+                                }
                               });
-                            }
-                          } else {
-                            if (global.vidIdx < posts.length - 1) {
-                              setState(() {
-                                global.vidIdx++;
-                              });
-                            }
-                          }
-                          //widget.createState();
-                        },
+                            },
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ])
-              ],
-            );
-            // return ListView.builder(
-            //     shrinkWrap: true,
-            //     itemCount: posts.length,
-            //     itemBuilder: (context, index) {
-            //       VideoPost post = posts[index];
-            //       return VideoPostWidget(post);
-            //     });
+                    ])
+                  ]);
+                });
           } else {
             return Container();
           }
