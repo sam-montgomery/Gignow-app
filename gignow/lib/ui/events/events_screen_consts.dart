b30ebd68a3748366dd1f84855051f595cf6dcbcb
Ronan Sands <rsands04@qub.ac.uk>
2021-12-10 17:07:46 +0000

    Event Bounding Box Error Fixes

--- gignow/lib/ui/events/events_screen_consts.dart
+++ gignow/lib/ui/events/events_screen_consts.dart
@@ -658,7 +658,7 @@ Container generateOpenEventTile(
                           ),
                         )
                       : Padding(
-                          padding: const EdgeInsets.all(8.0),
+                          padding: const EdgeInsets.all(6.0),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             mainAxisAlignment: MainAxisAlignment.center,
